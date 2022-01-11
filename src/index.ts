bbb90a42920bfa9ea11351ae12ac2f2dee7850c8
uginget <etienne.gingras@ville.montreal.qc.ca>
2022-01-11 15:57:47 -0500

    adjust eslint base and node

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { prettierCheck, prettierFix } from './prettier';
 export { eslintCheck, eslintFix } from './eslint';
+export { tslintCheck, tslintFix } from './tslint';
 export * from './models';
